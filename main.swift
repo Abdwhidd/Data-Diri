0f2d58ece240b2408791e8f617221a963c70f7e2
Abdul Wahid Kahar <76991016+Abdwhidd@users.noreply.github.com>
2022-06-09 06:04:28 +0700

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  Data Diri
+//
+//  Created by Wahid on 08/06/22.
+//
+
+import Foundation
+
+// MARK: Membuat form data diri.
+print("Selamat Datang di Dicoding Academy")
+
+let firstName = "Abdul wahid", lastName = "kahar"
+let fullName = firstName + " " + lastName
+let address = "Bandung"
+let job = "iOS Developer"
+let age = 0b10001
+
+print("----------------------------------")
+
+// MARK: Mencetak program yang Anda buat ke dalam Terminal.
+print("Apakah kalian tahu \(fullName)?")
+print("\(firstName) adalah seorang \(job).")
+print("Saat ini ia berumur \(age) dan bertempat tinggal di \(address).")
+
+print("----------------------------------")
+
